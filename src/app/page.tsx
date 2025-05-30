1901f9553242da9454af6f6f80b9aed77e202372
Nishchit-Dev <nishchitpatel84@gmail.com>
2025-05-29 23:04:17 -0500

    feat: landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,53 @@
-"use client";
-
-import AnimatedGradientText from "@/UI/AnimatedGradientText";
+'use client'
 
 export default function Home() {
-  return (
-    <div className="h-full">
-      <div className="min-h-screen flex items-center justify-center">
-        <AnimatedGradientText className="text-center">
-          Hello Lovable Devs
-        </AnimatedGradientText>
-      </div>
-    </div>
-  );
+    return (
+        <div className="h-full">
+            <div className="min-h-screen bg-black flex items-center justify-center overflow-hidden relative ">
+                {/* <div
+                    className="w-full h-full absolute"
+                    style={{
+                        background:
+                            'radial-gradient(ellipse 100% 30% at bottom, #F9F9FB 100%,transparent 100%)',
+                    }}
+                ></div>{' '} */}
+                <div
+                    className="w-full h-full absolute scale-100 "
+                    style={{
+                        background:
+                            'radial-gradient(ellipse 100% 100% at bottom, #000000 100%)',
+                    }}
+                ></div>
+                <div
+                    className="w-full h-full absolute"
+                    style={{
+                        background:
+                            'radial-gradient(ellipse 50% 70% at bottom,#32247A 95%, transparent 140%,transparent 60%,transparent 80%)',
+                    }}
+                ></div>{' '}
+                <div
+                    className="w-full h-full absolute"
+                    style={{
+                        background:
+                            'radial-gradient(ellipse 40% 80% at bottom, #2B1C83 70%, transparent 125%)',
+                    }}
+                ></div>{' '}
+                <div
+                    className="w-full h-full absolute"
+                    style={{
+                        background:
+                            'radial-gradient(ellipse 30% 60% at bottom, #8C80DB 80%, transparent 135%)',
+                    }}
+                ></div>
+                <div
+                    className="w-full h-full absolute"
+                    style={{
+                        background:
+                            'radial-gradient(ellipse 20% 40% at bottom, #F9F9FB 50%,transparent 120%)',
+                    }}
+                ></div>{' '}
+                <p className="text-center z-10 text-5xl font-extrabold text-white">Build Beautiful UI - Faster</p>
+            </div>
+        </div>
+    )
 }
