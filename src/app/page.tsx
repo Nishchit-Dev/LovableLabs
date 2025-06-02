66c420571a62b2883a9b7585a49e26099eb926bd
Nishchit-Dev <nishchitpatel84@gmail.com>
2025-06-02 01:41:46 -0500

    feat: added CTA for hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,9 +13,9 @@ export default function Home() {
                             'radial-gradient(ellipse 100% 30% at bottom, #F9F9FB 100%,transparent 100%)',
                     }}
                 ></div>{' '} */}
-                <div className="w-full absolute xl:h-[80%] lg:h-[80%] md:h-[60%] bottom-0">
+                <div className="w-full  absolute xl:h-[80%] lg:h-[80%] md:h-[60%] bottom-0">
                     <motion.div
-                        animate={{ scale: [1, 1.08, 0.8,1] }}
+                        animate={{ scale: [1, 1.08, 1] }}
                         transition={{
                             duration: 2,
                             repeat: Infinity,
@@ -28,7 +28,7 @@ export default function Home() {
                         }}
                     ></motion.div>
                     <motion.div
-                        animate={{ scale: [1, 1.1,1] }}
+                        animate={{ scale: [1, 1.15, 1] }}
                         transition={{
                             duration: 3,
                             repeat: Infinity,
@@ -41,7 +41,7 @@ export default function Home() {
                         }}
                     ></motion.div>{' '}
                     <motion.div
-                        animate={{ scale: [1, 1.14, 1],opacity:[1,0.7,1] }}
+                        animate={{ scale: [1, 1.18, 1], opacity: [1, 0.7, 1] }}
                         transition={{
                             duration: 4,
                             repeat: Infinity,
@@ -54,7 +54,7 @@ export default function Home() {
                         }}
                     ></motion.div>{' '}
                     <motion.div
-                        animate={{ scale: [1, 1.03,1],opacity:[1,0.8,1] }}
+                        animate={{ scale: [1, 1.08, 1], opacity: [1, 0.8, 1] }}
                         transition={{
                             duration: 3,
                             repeat: Infinity,
@@ -67,7 +67,7 @@ export default function Home() {
                         }}
                     ></motion.div>
                     <motion.div
-                        animate={{ scale: [1, 1.02,1],opacity:[1,0.75,1]  }}
+                        animate={{ scale: [1, 1.07, 1], opacity: [1, 0.75, 1] }}
                         transition={{
                             duration: 6,
                             repeat: Infinity,
@@ -80,10 +80,29 @@ export default function Home() {
                         }}
                     ></motion.div>{' '}
                 </div>
-
-                <p className="text-center z-10 text-5xl font-extrabold text-white">
-                    Build Beautiful UI - Faster
-                </p>
+                <div className=" p-5  flex flex-col gap-2 opacity-75 flex-1 justify-center items-center">
+                    <p className="text-center z-10 text-5xl font-extrabold text-white ">
+                        Build <motion.span>5x Faster</motion.span>. Design{' '}
+                        <motion.span>Better</motion.span>. Ship{' '}
+                        <motion.span>Sooner</motion.span>.
+                    </p>
+                    <p className="z-10 text-white font-regular text-lg">
+                        A modern, accessible UI component library for React
+                        projects.
+                    </p>
+                    <div className="flex flex-row gap-4 pt-2">
+                        <div className="bg-black/70 px-4 py-2 rounded-full cursor-pointer">
+                            <p className="z-10 text-white font-regular text-lg">
+                                Get Started 🚀
+                            </p>
+                        </div>
+                        <div className="bg-black/70 bg-opacity-75 px-4 py-2 rounded-full cursor-pointer">
+                            <p className="z-10 text-white font-regular text-lg">
+                                docs 📄
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
