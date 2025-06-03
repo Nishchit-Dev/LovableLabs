4b95f3f003e0be80a41c852346d5fd9c130ce5f5
Nishchit-Dev <nishchitpatel84@gmail.com>
2025-06-03 00:21:04 -0500

    feat: added new component.

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,14 +1,11 @@
-import { GridBackground } from "../docs/constants/content/code/GridBackground"
-
+import { CircularAnimation } from '../docs/constants/content/code/circularAnimation'
 
 const DevTools =
     process.env.NODE_ENV === 'development'
         ? () => (
-              <GridBackground full centered className="bg-white" overlay boxSize={42}>
-                  <div className="h-10 w-20 bg-blue-500 rounded-lg flex justify-center items-center bg-wh">
-                      <p className="text-center text-white">Button</p>
-                  </div>
-              </GridBackground>
+              <div className='w-full h-screen flex justify-center items-center'>
+                  <CircularAnimation />
+              </div>
           )
         : () => <div>404 | Not Found</div>
 
