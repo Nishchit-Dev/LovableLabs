5f5c8de6058da04e91437f8020b66b965298c20f
Nishchit-Dev <nishchitpatel84@gmail.com>
2025-06-17 23:40:20 -0500

    feat: added shadcn + intergrated tabs in docSection.

--- src/app/docs/components/DocSection.tsx
+++ src/app/docs/components/DocSection.tsx
@@ -6,6 +6,12 @@ import { motion } from 'framer-motion'
 import './markdown.css'
 import { Copy, Lock, Check, Eye, EyeClosed } from 'lucide-react'
 import Countdown from '@/app/utils/countdown'
+import {
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+} from '@/app/components/mirco-components/tabs'
 
 type DocSectionProps = {
     content: DocContent
@@ -111,8 +117,12 @@ const CodeBlock = ({ code, language, codeSrc }: CodeBlockProps) => {
                                     overflow: 'auto',
                                     backgroundColor: '#1e1e1e',
                                     scrollbarWidth: 'thin',
-                                    scrollbarColor: 'oklch(60.6% 0.25 292.717) #000',
-                                    maxHeight: showExpandButton && !isExpanded ? '200px' : 'none',
+                                    scrollbarColor:
+                                        'oklch(60.6% 0.25 292.717) #000',
+                                    maxHeight:
+                                        showExpandButton && !isExpanded
+                                            ? '200px'
+                                            : 'none',
                                     position: 'relative',
                                 }}
                             >
@@ -125,10 +135,11 @@ const CodeBlock = ({ code, language, codeSrc }: CodeBlockProps) => {
                                     return (
                                         <div key={i} {...restLineProps}>
                                             {line.map((token, key) => {
-                                                const tokenProps = getTokenProps({
-                                                    token,
-                                                    key,
-                                                })
+                                                const tokenProps =
+                                                    getTokenProps({
+                                                        token,
+                                                        key,
+                                                    })
                                                 return (
                                                     <span
                                                         key={key}
@@ -198,36 +209,12 @@ const CodeBlock = ({ code, language, codeSrc }: CodeBlockProps) => {
     )
 }
 
-const DocSection = ({ content }: DocSectionProps) => {
+const PreviewTab = ({ content }: DocSectionProps) => {
     return (
-        <motion.div
-            layout="size"
-            initial={{
-                opacity: 0,
-                y: 40,
-            }}
-            animate={{
-                opacity: 1,
-                y: 0,
-            }}
-            transition={{
-                type: 'tween',
-                ease: [0.4, 0, 0.2, 1],
-                delay: 0.6,
-                duration: 0.8,
-            }}
-            className="text-[var(--font-white)] overflow-hidden"
-        >
-            <h1 className="text-3xl font-bold mb-3">{content.title}</h1>
-            <p className="text-[var(--font-gray)] mb-10">
-                {content.description}
-            </p>
-
-            {content.preview}
-
+        <div className="w-full">
             {new Date(content.releaseDate || '').getTime() > Date.now() ? (
                 <div>
-                    <div className="flex flex-col justify-center items-center gap-5">
+                    <div className="flex flex-col ww-full justify-center items-center gap-5">
                         <>
                             <Lock size={32} />
                             <p className="text-lg">
@@ -286,6 +273,137 @@ const DocSection = ({ content }: DocSectionProps) => {
                     </div>
                 ))
             )}
+        </div>
+    )
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const VariantsTab = ({ content }: DocSectionProps) => {
+    return (
+        <div className="w-full">
+            {new Date(content.releaseDate || '').getTime() > Date.now() ? (
+                <div>
+                    <div className="flex flex-col ww-full justify-center items-center gap-5">
+                        <>
+                            <Lock size={32} />
+                            <p className="text-lg">
+                                Hold tight! This component will go live on.
+                            </p>
+                        </>
+
+                        <div className="flex">
+                            {content.releaseDate ? (
+                                <Countdown targetDate={content.releaseDate} />
+                            ) : (
+                                <></>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                content.sections.map((section, index) => (
+                    <div key={index} className="flex flex-row">
+                        <div className="w-[0.5px] bg-white/20 self-stretch lg:mr-10 mr-4">
+                            <div className="w-[5px] h-[35px] rounded-r-2xl bg-violet-500"></div>
+                        </div>
+                        <div className="mb-16 w-full min-w-0">
+                            {section.title && (
+                                <div className="mb-6">
+                                    <h2
+                                        id={section.title
+                                            .toLowerCase()
+                                            .replace(/\s+/g, '-')}
+                                        className="text-2xl font-bold text-[var(--font-white)] mb-2 flex items-center"
+                                    >
+                                        {section.title}
+                                    </h2>
+                                    {section.description && (
+                                        <p className="text-[var(--font-gray)] text-md font-light">
+                                            {section.description}
+                                        </p>
+                                    )}
+                                </div>
+                            )}
+
+                            <div className="markdown-content text-[var(--font-white)] text-sm">
+                                <ReactMarkdown>{section.content}</ReactMarkdown>
+                            </div>
+
+                            {section.code && (
+                                <div className="mt-6 bg-[#1E1E1E] rounded-md overflow-x-auto">
+                                    <CodeBlock
+                                        code={section.code}
+                                        codeSrc={section.codeSrc}
+                                        language={'jsx'}
+                                    />
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                ))
+            )}
+        </div>
+    )
+}
+
+const TabsMerger = ({ content }: DocSectionProps) => {
+    return (
+        <>
+            <Tabs defaultValue="Preview" className="">
+                <TabsList>
+                    <TabsTrigger
+                        value="Preview"
+                        className="cursor-pointer opacity-75 hover:opacity-100 active:opacity-100"
+                    >
+                        Preview
+                    </TabsTrigger>
+                    <TabsTrigger
+                        value="Variants"
+                        className="cursor-pointer opacity-75 hover:opacity-100 active:opacity-100"
+                    >
+                        Variants
+                    </TabsTrigger>
+                </TabsList>
+
+                <TabsContent value="Preview" className="">
+                    <PreviewTab content={content} />
+                </TabsContent>
+                <TabsContent value="Variants">
+                    <VariantsTab content={content} />
+                </TabsContent>
+            </Tabs>
+        </>
+    )
+}
+
+const DocSection = ({ content }: DocSectionProps) => {
+    return (
+        <motion.div
+            layout="size"
+            initial={{
+                opacity: 0,
+                y: 40,
+            }}
+            animate={{
+                opacity: 1,
+                y: 0,
+            }}
+            transition={{
+                type: 'tween',
+                ease: [0.4, 0, 0.2, 1],
+                delay: 0.6,
+                duration: 0.8,
+            }}
+            className="text-[var(--font-white)] overflow-hidden"
+        >
+            <h1 className="text-3xl font-bold mb-3">{content.title}</h1>
+            <p className="text-[var(--font-gray)] mb-10">
+                {content.description}
+            </p>
+
+            {content.preview}
+
+            <TabsMerger content={content} />
         </motion.div>
     )
 }
