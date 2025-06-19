6169a92a4b7fe480796fe3814f436c525a16224a
Nishchit-Dev <nishchitpatel84@gmail.com>
2025-06-19 14:08:54 -0500

    feat: added varaint for DotMartixCountDown + Preview of Varaint

---
+++ src/app/docs/constants/content/Builds/Variants/DotMatrixCountDown/DotMatrixCountDown.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { GridBackground } from '../../../code/GridBackground'
+import { CountdownTimer } from '../../../code/DotMatrixCountdownTimer'
+
+export const DotMatrixCountdownLight = () => {
+    return (
+        <div className="mb-10">
+            <div className="rounded-xl p-6 flex items-center justify-center  h-full border border-[#333]">
+                <GridBackground
+                    full
+                    overlay
+                    centered
+                    boxSize={36}
+                    className="min-h-[350px] w-full p-12"
+                >
+                    <CountdownTimer
+                        initialTime={450000}
+                        pixelSize={6}
+                        onComplete={() => console.log('Timer completed!')}
+                    />
+
+                    <p className="text-black/70 text-center">
+                        Hour : Minute : Second{' '}
+                    </p>
+                </GridBackground>
+            </div>
+        </div>
+    )
+}
+
+export const DotMatrixCountdownDark = () => {
+    return (
+        <div className="mb-10">
+            <div className="rounded-xl p-6 flex items-center justify-center  h-full border border-[#333]">
+                <GridBackground
+                    full
+                    overlay
+                    centered
+                    boxSize={36}
+                    className="min-h-[350px] w-full p-12"
+                >
+                    <CountdownTimer
+                        initialTime={100520}
+                        pixelSize={6}
+                        dark
+                        onComplete={() => console.log('Timer completed!')}
+                    />
+                    <p className="text-black/70 text-center">
+                        Hour : Minute : Second{' '}
+                    </p>
+                </GridBackground>
+            </div>
+        </div>
+    )
+}
