865ee5d734cfb1b5695f87c8ed5bde1761ce07d8
Nishchit-Dev <nishchitpatel84@gmail.com>
2025-06-19 01:03:08 -0500

    feat: added DottedBg varaint build

---
+++ src/app/docs/constants/content/Builds/Variants/DottedBackground/DottedBackground.tsx
@@ -0,0 +1,28 @@
+import { DottedBackground } from '../../../code/DottedBackground'
+
+export const DottedBgVaraint = () => {
+    return (
+        <>
+            <div className="rounded-xl p-6 flex items-center justify-center max-h-[420px] h-[420px]  border border-[#333]">
+                <DottedBackground dark centered boxSize={36} full>
+                    <p className="text-white"> Centered Text with Dark Bg</p>
+                </DottedBackground>
+            </div>
+        </>
+    )
+}
+
+export const DottedBgVaraintOverLay = () => {
+    return (
+        <>
+            <div className="rounded-xl p-6 flex items-center justify-center max-h-[420px] h-[420px]  border border-[#333]">
+                <DottedBackground dark centered boxSize={46} overlay full>
+                    <p className="text-white">
+                        {' '}
+                        Centered Text with Overlay Dark Bg and Box size 46
+                    </p>
+                </DottedBackground>
+            </div>
+        </>
+    )
+}
