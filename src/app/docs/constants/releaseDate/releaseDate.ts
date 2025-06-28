2f5d5d58833a898a047dca39df8ee253d051e903
Nishchit-Dev <nishchitpatel84@gmail.com>
2025-06-28 09:40:18 -0500

    feat: added release date for ScrollWaves

--- src/app/docs/constants/releaseDate/releaseDate.ts
+++ src/app/docs/constants/releaseDate/releaseDate.ts
@@ -8,4 +8,5 @@ export const releaseDate = {
     motionTextReveal: '2025-06-20T23:59:59',
     template: '2200-06-20T23:59:59',
     framerTextAnimation:'2025-06-22T23:59:59',
+    scrollWave:'2025-07-01T23:59:59'
 }
